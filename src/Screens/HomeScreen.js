b0c4d3e86593bd28b45a8e43980ec37a0c1067f5
Simba Zhang <solderzzc@gmail.com>
2019-02-03 12:49:25 -0800

    A working version with react-native-swiper.

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -7,7 +7,8 @@ import Icon2 from 'react-native-vector-icons/MaterialIcons';
 import anim from '../../assets/lottie/play.json';
 import Animation from 'lottie-react-native';
 import { Actions } from 'react-native-router-flux';
-
+import { UltimateListView, UltimateRefreshView } from 'react-native-ultimate-listview';
+import Swiper from 'react-native-swiper';
 
 class HomeScreen extends Component {
     constructor(props) {
@@ -32,7 +33,13 @@ class HomeScreen extends Component {
     render() {
         const like = this.state.liked ? 'red' : 'white';
         return (
-      <View style={styles.container}>
+            <View style={styles.slide1}>
+            <Swiper style={styles.wrapper} 
+                showsButtons={false}
+                horizontal={false}
+                loop={false}
+            >
+            <View style={styles.container}>
                 <Video  source={require('../video.mp4')} resizeMode="cover" style={StyleSheet.absoluteFill} />
                 <View style={styles.full}>
                         <View style={{flex:.5,justifyContent:'flex-end'}}>
@@ -93,7 +100,19 @@ class HomeScreen extends Component {
                     </View>
                     </View>
                 </View>
-      </View>
+            </View>
+            <View style={styles.slide1}>
+                <Text style={styles.text}>Hello Swiper</Text>
+            </View>
+            <View style={styles.slide2}>
+                <Text style={styles.text}>Beautiful</Text>
+            </View>
+            <View style={styles.slide3}>
+                <Text style={styles.text}>And simple</Text>
+            </View>
+        </Swiper>
+                <Text style={styles.text}>And simple</Text>
+        </View>
         );
     }
 }
@@ -166,9 +185,32 @@ const styles = StyleSheet.create({
         width:40,
         height:40,
         borderRadius:40/2
+    },
+    wrapper: {
+    },
+    slide1: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#9DD6EB',
+    },
+    slide2: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#97CAE5',
+    },
+    slide3: {
+      flex: 0.1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#92BBD9',
+    },
+    text: {
+      color: '#fff',
+      fontSize: 30,
+      fontWeight: 'bold',
     }
-    
-   
   });
 
 export default HomeScreen;
